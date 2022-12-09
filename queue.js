c8c4bf5fa1cbff58e9a572a46ef8bbfd2be189c5
John <antarcticdepths@gmail.com>
2022-12-09 17:00:49 +0100

    initial class shenanigans

--- queue.js
+++ queue.js
@@ -1,34 +1,55 @@
 class Queue {
-  constructor() {
+  constructor(name) {
+    this.name = name;
     this.queue = [];
   }
 
   pushHead(element) {
     this.queue.unshift(element);
+    this.saveQueue(this.name, this.queue);
+  }
+
+  saveQueue() {
+    localforage
+      .setItem(this.name, this.queue)
+      .then(function (value) {})
+      .catch(function (err) {
+        console.log(err);
+      });
+  }
+
+  getQueue() {
+    localforage
+      .getItem(this.name)
+      .then(function (value) {
+        console.log(value);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   }
 }
 
 const fifo = new Queue("Jason");
 fifo.pushHead("Pushed borzus");
+fifo.getQueue();
 
-function saveQueue() {
-  localforage
-    .setItem(fifo.toString(), fifo.queue)
-    .then(function (value) {})
-    .catch(function (err) {
-      console.log(err);
-    });
-}
+// function saveQueue(name, queue) {
+//   localforage
+//     .setItem(name, queue)
+//     .then(function (value) {})
+//     .catch(function (err) {
+//       console.log(err);
+//     });
+// }
 
-function getQueue() {
-  localforage
-    .getItem(fifo.toString())
-    .then(function (value) {
-      console.log(value);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-}
-getQueue();
-saveQueue();
+// function getQueue() {
+//   localforage
+//     .getItem(fifo.toString())
+//     .then(function (value) {
+//       console.log(value);
+//     })
+//     .catch(function (err) {
+//       console.log(err);
+//     });
+// }
