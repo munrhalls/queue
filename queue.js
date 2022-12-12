2b763d529b926283e5d62c180c27926d728f9614
John <antarcticdepths@gmail.com>
2022-12-12 23:12:02 +0100

    async queue

--- queue.js
+++ queue.js
@@ -1,20 +1,29 @@
-class Queue {
-  constructor(name) {
-    this.store = localforage.createInstance({
-      name: name,
-    });
+(async function () {
+  class Queue {
+    constructor(name, getQueue) {
+      this.store = localforage.createInstance({
+        name: name,
+      });
+      this.getQueue = getQueue.bind(this);
+    }
+
+    async test() {
+      const queue = await this.getQueue();
+      console.log(queue);
+    }
   }
 
-  async test() {
-    console.log(await this.getQueue());
-  }
-
-  getQueue() {
+  function getQueue() {
     return new Promise((resolve) => {
       return this.store.getItem("queue").then((queue) => resolve(queue));
     });
   }
-}
 
-const Test = new Queue("Test");
-console.log(Test.test());
+  function makeQueue(name) {
+    return new Queue(name, getQueue);
+  }
+
+  const Test = makeQueue("Test");
+  const queue = await Test.getQueue();
+  console.log(queue);
+})();
