15e06cd6f28bc67941bde023bf5a12908ac65ac9
John <antarcticdepths@gmail.com>
2022-12-13 15:56:54 +0100

    make lf instance func

--- queue.js
+++ queue.js
@@ -7,20 +7,39 @@
       this.#lf_instance = lf_instance;
     }
 
-    async showQueue() {
-      const queue = await this.#lf_instance.getItem("queue").then((val) => val);
+    async getAll() {
+      const queue = await this.#lf_instance
+        .getItem("queue")
+        .then((val) => val)
+        .catch((err) => console.log(err));
+
       console.log(queue);
+      return queue;
     }
   }
 
-  async function queueFactory(name) {
-    const lf_instance = localforage.createInstance({
-      name: name,
-    });
+  async function makeLFInstance() {
+    const lf_instance = new Promise(
+      (resolve) => {
+        resolve(
+          localforage.createInstance({
+            name: name,
+          })
+        );
+      },
+      (err) => console.log(err)
+    );
+    return lf_instance;
+  }
+
+  async function makeQueue(name) {
+    const lf_instance = await makeLFInstance();
+
+    console.log(lf_instance);
 
     const queue = await new Promise((resolve) => {
       lf_instance.getItem("queue").then((val) => resolve(val));
-    });
+    }).catch((err) => console.log(err));
 
     if (!queue)
       await new Promise((resolve) =>
@@ -32,7 +51,10 @@
   }
 
   const Test = await new Promise((resolve) => {
-    queueFactory("Test").then((queue) => resolve(queue));
+    makeQueue("Test").then((queue) => resolve(queue));
   });
-  console.log(Test);
+
+  const Test2 = await makeQueue("Second test");
+  Test2.getAll();
+  Test.getAll();
 })();
