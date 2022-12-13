a74859a8f73a3910ea0bbd5cf151908edba9aeec
John <antarcticdepths@gmail.com>
2022-12-13 20:14:51 +0100

    refactor

--- queue.js
+++ queue.js
@@ -27,19 +27,26 @@
         .catch((err) => console.log(err));
     }
 
-    async push_head(element) {
+    async update_queue(element) {
       const queue = await this.get_queue();
       const id = this.generateId(queue.length);
-      const next = queue[1] || "";
+      const nextId = queue[1] ? queue[1].id : "";
+
       const newHead = {
         id: id,
-        next: next,
+        next: nextId,
         value: element,
       };
+
       if (queue[0]) queue[0].prev = newHead.id;
       queue.unshift(newHead);
+      return queue;
+    }
 
-      await this.set_queue(queue);
+    async push_head(element) {
+      const update = await this.update_queue(element);
+
+      await this.set_queue(update);
     }
 
     async pop_tail() {
