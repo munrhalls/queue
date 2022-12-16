710360a899fff1b2eb5607dbca27be7b4c422132
John <antarcticdepths@gmail.com>
2022-12-16 09:44:05 +0100

    seed

--- seed.js
+++ seed.js
@@ -1,3 +1,23 @@
-const seed = [];
+const seed = [[]];
+const names = [
+  "Jason",
+  "Blaise",
+  "Zalzia",
+  "Vincyus",
+  "Valvarg",
+  "Ylensia",
+  "Ajmina",
+];
+
+let y = 0;
+for (let i = 0; i < 400; i++) {
+  if (y > 6) y = 0;
+  seed[i] = {
+    name: names[y],
+    photo: Math.random(),
+    device: y % 2 === 0 ? "Android" : "iOS",
+  };
+  y++;
+}
 
 export default seed;
